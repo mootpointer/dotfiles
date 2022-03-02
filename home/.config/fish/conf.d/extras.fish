989b714337b98ecf81a72699ae6bf65656dd8911
Andrew Harvey <andrew@mootpointer.com>
2022-03-02 11:25:06 +1100

    Update fish extras

--- home/.config/fish/conf.d/extras.fish
+++ home/.config/fish/conf.d/extras.fish
@@ -1,6 +1,5 @@
-. "$HOME/.homesick/repos/homeshick/homeshick.fish"
-test -s "$HOME/.kiex/scripts/kiex.fish"; and source "$HOME/.kiex/scripts/kiex.fish"
+set -x EDITOR 'code -w'
 
 set -x PATH /Users/harvand/.cargo/bin $PATH
 
-set -x PATH /Users/harvand/.cargo/bin $PATH
+source ~/.asdf/asdf.fish
